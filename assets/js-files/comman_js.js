ec8583d663542e0220f306c610cd099b38c28c2c
aditya tripathi <adityawebiwork@gmail.com>
2025-04-15 01:18:59 -0400

    update foter

--- assets/js-files/comman_js.js
+++ assets/js-files/comman_js.js
@@ -1,33 +1,37 @@
-// Password eye script start
-const logPasswordInput = document.getElementById("logInputPassword");
-const logPassEye = document.getElementById("logPassEye");
+document.addEventListener("DOMContentLoaded", function () {
+  // Password eye script start
+  const logPasswordInput = document.getElementById("logInputPassword");
+  const logPassEye = document.getElementById("logPassEye");
 
-logPassEye.addEventListener("click", function () {
-  if (logPasswordInput.type === "password") {
-    logPasswordInput.type = "text";
-    logPassEye.classList.remove("bi-eye-fill");
-    logPassEye.classList.add("bi-eye-slash-fill");
-  } else {
-    logPasswordInput.type = "password";
-    logPassEye.classList.remove("bi-eye-slash-fill");
-    logPassEye.classList.add("bi-eye-fill");
+  if (logPasswordInput && logPassEye) {
+    logPassEye.addEventListener("click", function () {
+      if (logPasswordInput.type === "password") {
+        logPasswordInput.type = "text";
+        logPassEye.classList.remove("bi-eye-fill");
+        logPassEye.classList.add("bi-eye-slash-fill");
+      } else {
+        logPasswordInput.type = "password";
+        logPassEye.classList.remove("bi-eye-slash-fill");
+        logPassEye.classList.add("bi-eye-fill");
+      }
+    });
   }
-});
+  // Password eye script end
 
-// Password eye script end
+  // Comman header link
+  const navLinks = document.querySelectorAll("#navbarNav .nav-link");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navLinks.forEach((nav) => nav.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
 
-// comman header link
-const navLinks = document.querySelectorAll("#navbarNav .nav-link");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", function () {
-    // Remove active class from all links
-    navLinks.forEach((nav) => nav.classList.remove("active"));
-
-    // Add active class to the clicked link
-    this.classList.add("active");
+  // Initialize all tooltips on the page
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
   });
 });
-
-// comman header link
-
