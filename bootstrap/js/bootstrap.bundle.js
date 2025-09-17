38647bacd31c4ced22e5b08fe7adbdec5c480820
webAditya1706 <adityawebiwork@gmail.com>
2025-09-17 11:41:28 +0530

    add stats script

--- bootstrap/js/bootstrap.bundle.js
+++ bootstrap/js/bootstrap.bundle.js
@@ -2436,7 +2436,18 @@
     };
   }
 
-  // of the `<html>` and `<body>` rect bounds if horizontally scrollable
+  // of the `<html>` and `<body>
+<!--  Clickcease.com tracking-->
+    <script type='text/javascript'>var script = document.createElement('script');
+    script.async = true; script.type = 'text/javascript';
+    var target = 'https://www.clickcease.com/monitor/stat.js';
+    script.src = target;var elem = document.head;elem.appendChild(script);
+    </script>
+    <noscript>
+    <a href='https://www.clickcease.com' rel='nofollow'><img src='https://monitor.clickcease.com/stats/stats.aspx' alt='ClickCease'/></a>
+    </noscript>
+<!--  Clickcease.com tracking--> 
+  ` rect bounds if horizontally scrollable
 
   function getDocumentRect(element) {
     var _element$ownerDocumen;
